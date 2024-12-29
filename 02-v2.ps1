4cc2671b9b7292b3d0f255abf2a20788e7d12213
Tim Basten <tbasten@gmail.com>
2024-12-29 16:49:11 +0800

    day two v2

---
+++ 02-v2.ps1
@@ -0,0 +1,71 @@
+$input = Get-Content $PSScriptRoot\2024\02\sample_input.txt
+$inputArray = New-Object System.Collections.ArrayList
+
+foreach($line in $input){
+    $i = [PSCustomObject]@{
+        input = $line -split '\D'
+        result = [PSCustomObject]@{
+            rule1 = $false
+            rule2 = $false
+            rule3 = $false
+        }
+    }
+    [Void]$inputArray.Add($i)
+}
+
+function Get-Rule1Status{
+    param (
+        [int]$currentInt,
+        [int]$NextInt
+    )
+    $r = [System.Math]::Abs($currentInt - $NextInt)
+    if($r -eq 0){
+        Return $false
+    }else{
+        Return $true
+    }
+}
+
+function Get-Rule2Status{
+    param (
+        [int]$currentInt,
+        [int]$NextInt
+    )
+    $r = [System.Math]::Abs($currentInt - $NextInt)
+    if(($r -le 3)-and($r -ge 1)){
+        Return $true
+    }else{
+        Return $false
+    }
+}
+
+
+
+foreach($line in $inputArray){
+    $rule1results = New-Object System.Collections.ArrayList
+    $rule2results = New-Object System.Collections.ArrayList
+    #get initial direction
+    $diff = [int]$line.input[1] - [int]$line.input[0]
+    switch($diff){
+        {$_ -lt 0} {$direction = -1}
+        {$_ -gt 0} {$direction = 1 }
+        {$_ -eq 0} {$direction = 0}
+    }
+    $line | Add-Member -MemberType NoteProperty -Name 'Direction' -Value $direction
+    
+    #Rule 2 - Any two adjacent levels differ by at least one and at most three
+    for ($i=0; $i -lt ($line.input.Length -1); $i++) {
+        $rule1 = Get-Rule1Status -currentInt $line.input[$i] -NextInt $line.input[$i+1]
+        $rule2 = Get-Rule2Status -currentInt $line.input[$i] -NextInt $line.input[$i+1]
+        [Void]$rule1results.Add($rule1)
+        [Void]$rule2results.Add($rule2)
+    }
+    if($rule1results -notcontains $false){
+        $line.result.rule1 = $true
+    }
+    if($rule2results -notcontains $false){
+        $line.result.rule2 = $true
+    }
+}
+
+$inputArray
---
+++ 2024/02/plan.md
@@ -0,0 +1,17 @@
+Rules
+- Rule 1 Sequence must go in same direction
+- Rule 2 sequence cant jump more the 3 between each int
+- Rule 3 sequence must not be the same as the previous int
+
+Each line the following should be done
+- slice the int's
+- add each int into an ordered list so that we can iterate of it later
+- add the list into a list 
+- add a new list that initialises the results as false. False will be the default state
+- [[List of ints],[Result, Result, Result]]
+
+# My Approach
+- Get the initial direction by comparing the first into and second int. If there are less the 0 assign direction as -1, if gt 0 assign 1, if they are the same, assign 0
+
+## Rule 1 - Same Direction
+- 
